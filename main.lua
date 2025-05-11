9b75be45a3be410966a6a1286c5d594fba230be5
(V)ariable <minecraftawesomed@gmail.com>
2025-05-11 10:49:03 -0400

    Update main.lua
    soundproof for future. wait i mean

--- main.lua
+++ main.lua
@@ -16,10 +16,14 @@ local NAMESPACE = "ANXvariable"
 local initialize = function() 
     local hunter = Survivor.new(NAMESPACE, "hunter")
 
-    -- Utility function for getting sprite paths concisely
+    -- Utility function for getting paths concisely
     local load_sprite = function (id, filename, frames, orig_x, orig_y, speed, left, top, right, bottom) 
         local sprite_path = path.combine(PATH, "Sprites",  filename)
         return Resources.sprite_load(NAMESPACE, id, sprite_path, frames, orig_x, orig_y, speed, left, top, right, bottom)
+    end
+    local load_sound = function (id, filename)
+        local sound_path = path.combine(PATH, "Sounds", filename)
+        return Resources.sfx_load(NAMESPACE, id, sound_path)
     end
     
     -- Load the common survivor sprites into a table
